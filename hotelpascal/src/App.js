53fa031260e28d9adb193864fa6a46a04b0449bb
Nnamdi Okeke <onnamdialex@gmail.com>
2023-02-02 18:40:59 +0100

    :zap: navbar menu for media screens done!

--- hotelpascal/src/App.js
+++ hotelpascal/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import "./App.scss";
 
 const App = () => {
   return (
--- hotelpascal/src/components/Navbar/Navbar.jsx
+++ hotelpascal/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
+
 import "./navbar.scss";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className="navbar">
       <div className="navbar-logo">Logo</div>
@@ -13,6 +17,28 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+
+      <div className="navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: "easeOut" }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            {["home", "about", "work", "skills", "contact"].map((item) => (
+              <ul>
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              </ul>
+            ))}
+          </motion.div>
+        )}
+      </div>
     </nav>
   );
 };
