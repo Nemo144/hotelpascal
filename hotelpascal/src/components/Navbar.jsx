5ce8828398e7a045b3fa90fa3132b9b2967365a1
Nnamdi Okeke <onnamdialex@gmail.com>
2023-02-02 14:56:40 +0100

    :tada: begin a project

---
+++ hotelpascal/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">Logo</div>
+      <ul className="navbar-links">
+        {["Home", "Rooms", "Gallery", "Contact", "About"].map((item) => (
+          <li className="">
+            <div />
+            <a href="/">{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
