4d3852f24572e591c3f95982d553bdee677525db
Nnamdi Okeke <onnamdialex@gmail.com>
2023-02-07 13:23:40 +0100

    :sparkles: scroll button feature wip ...

---
+++ hotelpascal/src/components/Scroll/ScrollButton.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { FaArrowCircleUp } from "react-icons/fa";
+
+const ScrollButton = () => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+
+    if (scrolled > 300) {
+      setVisible(true);
+    } else if (scrolled <= 300) {
+      setVisible(false);
+    }
+  };
+
+  const ScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  window.addEventListener("scroll", toggleVisible);
+
+  return (
+    <button>
+      <FaArrowCircleUp
+        onClick={ScrollToTop}
+        style={{ display: visible ? "inline" : "none" }}
+      />
+    </button>
+  );
+};
+
+export default ScrollButton;
