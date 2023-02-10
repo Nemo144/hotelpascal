ecaeb94ce7c543dffca8d26484a58562fb7a5c31
Nnamdi Okeke <onnamdialex@gmail.com>
2023-02-10 15:42:46 +0100

    :art: improve structure of code

--- hotelpascal/src/App.js
+++ hotelpascal/src/App.js
@@ -2,11 +2,21 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import ScrollButton from "./components/Scroll/ScrollButton";
 import "./App.scss";
+import Home from "./components/Home/Home";
+import Rooms from "./components/Rooms/Rooms";
+import About from "./components/About/About";
+import Gallery from "./components/Gallery/Gallery";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Home />
+      <Rooms />
+      <About />
+      <Gallery />
+      <Footer />
       <ScrollButton />
     </div>
   );
