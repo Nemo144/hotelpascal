f74184557c0811ae05fdef9e582f09f5760b14b0
Nnamdi Okeke <onnamdialex@gmail.com>
2023-02-16 20:54:09 +0100

    :construction: navbar wip/ reconstruction

--- hotelpascal/src/components/Navbar/Navbar.jsx
+++ hotelpascal/src/components/Navbar/Navbar.jsx
@@ -4,19 +4,34 @@ import { motion } from "framer-motion";
 
 import "./navbar.scss";
 
+const Menu = () => {
+  return (
+    <div>
+      <p id="#Home">Home</p>
+      <p id="#Rooms">Rooms</p>
+      <p id="#About">About</p>
+      <p id="#Gallery">Gallery</p>
+      <p if="#Contact"></p>
+    </div>
+  );
+};
+
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   return (
     <nav className="navbar">
       <div className="navbar-logo">Logo</div>
-      <ul className="navbar-links">
+      {/* <ul className="navbar-links">
         {["Home", "Rooms", "About", "Gallery", "Contact"].map((item) => (
           <li className="">
             <div />
             <a href="/">{item}</a>
           </li>
         ))}
-      </ul>
+      </ul> */}
+      <div className="navbar-links">
+        <Menu />
+      </div>
 
       <div className="navbar-menu">
         <HiMenuAlt4 onClick={() => setToggle(true)} />
